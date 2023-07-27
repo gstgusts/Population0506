da4bec7d393525fc98bd5d996f97306aedc48aa5
Gusts Linkevics <gusts@gst.lv>
2023-07-27 20:29:58 +0300

    Add project files.

---
+++ Population0506.Data/Models/Region.cs
@@ -0,0 +1,14 @@
+﻿namespace Population0506.Data.Models
+{
+    public class Region
+    {
+        public Region(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Population0506.Data/Services/DataAccessService.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Data.SqlClient;
+using Population0506.Data.Models;
+using System.Data;
+using System.Text;
+
+namespace Population0506.Data.Services
+{
+    public class DataAccessService
+    {
+        private readonly SqlConnectionProvider _connectionProvider;
+        public DataAccessService()
+        {
+            _connectionProvider = new SqlConnectionProvider();
+        }
+
+        public IEnumerable<Region> GetRegions()
+        {
+            return ExecuteSql("select * from Region");
+        }
+
+        public Region? GetRegionById(int id)
+        {
+            var parameters = new Dictionary<string, object>
+              {
+                  { "@id", id}
+              };
+
+            var regions = ExecuteSql("select * from Region where Id=@id", parameters);
+
+            return regions.FirstOrDefault();
+        }
+
+        public IEnumerable<Region> GetRegionsByName(string name)
+        {
+            var parameters = new Dictionary<string, object>
+              {
+                  { "@name", $"%{name}%"}
+              };
+
+            return ExecuteSql("select * from Region where Name like @name", parameters);
+        }
+
+        private IEnumerable<Region> ExecuteSql(string sql, Dictionary<string, object>? parameters = null)
+        {
+            var connection = _connectionProvider.GetConnection();
+
+            SqlCommand cmd = connection.CreateCommand();
+
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+
+            if (parameters != null && parameters.Any())
+            {
+                foreach (var parameter in parameters)
+                {
+                    cmd.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+            }
+
+            try
+            {
+                connection.Open();
+
+                var reader = cmd.ExecuteReader();
+
+                var items = new List<Region>();
+
+                while (reader.Read())
+                {
+                    items.Add(new Region(reader.GetInt32("Id"), reader.GetString("Name")));
+                }
+
+                connection.Close();
+
+                return items;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
